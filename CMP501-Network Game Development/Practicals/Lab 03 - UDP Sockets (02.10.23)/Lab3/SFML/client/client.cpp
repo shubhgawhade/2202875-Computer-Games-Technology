656118474fa1d6351bd40cff19e28f36ef221fdf
Shubh Gawhade <51043720+shubhgawhade@users.noreply.github.com>
2023-10-02 23:54:20 +0100

    Lab 03 CMP 501 - SFML server-client conversation

--- CMP501-Network Game Development/Practicals/Lab 03 - UDP Sockets (02.10.23)/Lab3/SFML/client/client.cpp
+++ CMP501-Network Game Development/Practicals/Lab 03 - UDP Sockets (02.10.23)/Lab3/SFML/client/client.cpp
@@ -11,14 +11,34 @@ int main()
 	socket.bind(55001);
 
 	// Send a message to 192.168.1.50 on port 55002
-	std::string message = "Hi, I am " + sf::IpAddress::getLocalAddress().toString();
-	socket.send(message.c_str(), message.size() + 1, "127.0.0.1", 55002);
+	//std::string message = "Hi, I am " + sf::IpAddress::getLocalAddress().toString();
+	//socket.send(message.c_str(), message.size() + 1, "192.168.0.122", 55002);
 
 	// Receive an answer (most likely from 192.168.1.50, but could be anyone else)
-	char buffer[1024];
+	//char* buffer = new char[1024];
+	//std::size_t received = 0;
+	//sf::IpAddress sender;
+	//unsigned short port;
+	//socket.receive(buffer, sizeof(buffer), received, sender, port);
+	//std::cout << sender << " said: " << buffer << std::endl;
+
 	std::size_t received = 0;
 	sf::IpAddress sender;
 	unsigned short port;
-	socket.receive(buffer, sizeof(buffer), received, sender, port);
-	std::cout << sender << " said: " << buffer << std::endl;
+	char buffer[1024];
+	
+	while (true)
+	{
+		printf("Type some text (\"quit\" to exit): ");
+		fflush(stdout);
+
+		// Read a line of text from the user.a
+		std::string line;
+		std::getline(std::cin, line);
+
+		socket.send(line.c_str(), line.size() + 1, "127.0.0.1", 55002);
+
+		socket.receive(buffer, sizeof(buffer), received, sender, port);
+		std::cout << sender << " said: " << buffer << std::endl;
+	}
 }
--- CMP501-Network Game Development/Practicals/Lab 03 - UDP Sockets (02.10.23)/Lab3/SFML/server/server.cpp
+++ CMP501-Network Game Development/Practicals/Lab 03 - UDP Sockets (02.10.23)/Lab3/SFML/server/server.cpp
@@ -11,14 +11,30 @@ int main()
 	socket.bind(55002);
 
 	// Receive a message from anyone
-	char buffer[1024];
+	//char* buffer = new char[1024];
+	//std::size_t received = 0;
+	//sf::IpAddress sender;
+	//unsigned short port;
+	//socket.receive(buffer, sizeof(buffer), received, sender, port);
+	//std::cout << sender << " said: " << buffer << std::endl;
+	//
+	//std::cout << "Hi, I am " + sf::IpAddress::getLocalAddress().toString() << std::endl;
+	//
+	//// Send an answer
+	//std::string message = "Welcome " + sender.toString();
+	//socket.send(message.c_str(), message.size() + 1, sender, port);
+
 	std::size_t received = 0;
 	sf::IpAddress sender;
 	unsigned short port;
-	socket.receive(buffer, sizeof(buffer), received, sender, port);
-	std::cout << sender << " said: " << buffer << std::endl;
+	char buffer[1024];
 
-	// Send an answer
-	std::string message = "Welcome " + sender.toString();
-	socket.send(message.c_str(), message.size() + 1, sender, port);
+	while (true)
+	{
+		socket.receive(buffer, sizeof(buffer), received, sender, port);
+		std::cout << sender << " said: " << buffer << std::endl;
+
+		std::string message = buffer;
+		socket.send(message.c_str(), message.size() + 1, sender, port);
+	}
 }
