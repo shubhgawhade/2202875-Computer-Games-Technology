61eac5bcf57afa873e3ecf4e97c0ada40bda5c9b
Shubh Gawhade <51043720+shubhgawhade@users.noreply.github.com>
2023-10-11 14:59:12 +0100

    Lab 02 CMP502 - Lighting

---
+++ CMP502-Programming For Games/Practicals/Lab 04 - (11.10.23)/Tutorial 4/Engine/light_vs.hlsl
@@ -0,0 +1,46 @@
+// Light vertex shader
+// Standard issue vertex shader, apply matrices, pass info to pixel shader
+
+cbuffer MatrixBuffer : register(b0)
+{
+    matrix worldMatrix;
+    matrix viewMatrix;
+    matrix projectionMatrix;
+};
+
+struct InputType
+{
+    float4 position : POSITION;
+    float2 tex : TEXCOORD0;
+    float3 normal : NORMAL;
+};
+
+struct OutputType
+{
+    float4 position : SV_POSITION;
+    float2 tex : TEXCOORD0;
+    float3 normal : NORMAL;
+};
+
+OutputType main(InputType input)
+{
+    OutputType output;
+    
+    input.position.w = 1.0f;
+
+    // Calculate the position of the vertex against the world, view, and projection matrices.
+    output.position = mul(input.position, worldMatrix);
+    output.position = mul(output.position, viewMatrix);
+    output.position = mul(output.position, projectionMatrix);
+    
+    // Store the texture coordinates for the pixel shader.
+    output.tex = input.tex;
+
+	 // Calculate the normal vector against the world matrix only.
+    output.normal = mul(input.normal, (float3x3)worldMatrix);
+	
+    // Normalize the normal vector.
+    output.normal = normalize(output.normal);
+
+    return output;
+}
