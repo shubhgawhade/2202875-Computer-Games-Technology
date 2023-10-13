0d88f9fd8f42cb301d4d98b8f9c42723e9945659
Shubh Gawhade <51043720+shubhgawhade@users.noreply.github.com>
2023-10-13 15:07:53 +0100

    Lab 04 MAT 501 - Priority Selector

---
+++ MAT501-Applied Mathematics and Artificial Intelligence/Practicals/Lab 04 - Behaviour Trees and Fuzzy Logic/Behviour Trees/Behaviour Trees/Scripts/Decorator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Decorator : SelectorNode
+{
+    [SerializeField] float runTime = 5f;
+    float elapsedTime = 0f;
+    protected override void NodeFunction()
+    {
+        // Update elapsed time
+        elapsedTime += Time.deltaTime;
+
+        // If the node is allowed to run
+        if (elapsedTime < runTime)
+        {
+            // Get first child node (assume others don't exist)
+            TreeNode child = childNodes[0];
+
+            // Start Node - This does nothing if its already running
+            child.StartNode();
+
+            // Run the Node and get its state
+            State childState = child.RunNode();
+
+            // If the child node stops running, reset the timer and update the decorator with the child nodes state
+            // if(childState != State.RUNNING)
+            // {
+            //     this.state = childState;
+            //     elapsedTime = 0f;
+            // }
+        }
+        else
+        {
+            // If we run out of time, assume node is successful and force this state into the child node
+            this.state = State.SUCCESS;
+            childNodes[0].ForceStateChange(State.SUCCESS);
+            elapsedTime = 0f;
+        }
+    }
+}
